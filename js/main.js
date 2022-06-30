cdefd6ef5d2f8851b800d4c408b2b6c10806039a
Ignacio Alimena Farayi <Alimenaignacio@gmail.com>
2022-06-30 13:46:00 -0300

    simulador

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+let entrada = "";
+
+while (entrada != "ESC") {
+    entrada = prompt ("ingrese los siguientes datos para un descuento: NOMBRE");
+    entrada = prompt ("ingrese los siguientes datos para un descuento: EMAIL");
+    entrada = prompt ("ingrese los siguientes datos para un descuento: DIRECCION");
+    entrada = prompt ("Ya tenes tu descuento! escribe ESC para salir");
+}
